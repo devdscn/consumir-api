15a92d074d3f635ca957633cb3f2bf5517d03661
David Nogueira <davidsergiossa@gmail.com>
2023-09-22 22:40:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Router } from 'react-router-dom/cjs/react-router-dom.min';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './store';
+
+import history from './services/history';
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Header />
+          <Routes />
+          <GlobalStyles />
+          <ToastContainer className="toast-container" autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
+}
