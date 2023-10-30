4416a48bc4253ec5aa018431d1982e6a02f01557
David Nogueira <davidsergiossa@gmail.com>
2023-10-30 16:12:08 -0300

    #Editando fotos

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -5,8 +5,10 @@ import PropTypes from 'prop-types';
 import { isEmail, isInt, isFloat } from 'validator';
 import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
+import { FaUserCircle, FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { Container } from '../../styles/GlobalStyles';
-import { Form } from './styled';
+import { Form, ProfilePicture, Title } from './styled';
 import Loading from '../../components/Loading';
 import axios from '../../services/axios';
 import history from '../../services/history';
@@ -14,13 +16,14 @@ import * as actions from '../../store/modules/auth/actions';
 
 export default function Aluno({ match }) {
   const dispacth = useDispatch();
-  const id = get(match, 'params.id', 0);
+  const id = get(match, 'params.id', '');
   const [nome, setNome] = useState('');
   const [sobrenome, setSobrenome] = useState('');
   const [email, setEmail] = useState('');
   const [idade, setIdade] = useState('');
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
+  const [foto, setFoto] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -32,6 +35,7 @@ export default function Aluno({ match }) {
 
         const { data } = await axios.get(`/alunos/${id}`);
         const Foto = get(data, 'Fotos[0].url', '');
+        setFoto(Foto);
 
         setNome(data.nome);
         setSobrenome(data.sobrenome);
@@ -120,18 +124,18 @@ export default function Aluno({ match }) {
       }
       setIsLoading(false);
     } catch (err) {
-      // caso token expirou
+      setIsLoading(false);
       const status = get(err, 'response.status', 0);
       const data = get(err, 'response.data', {});
-      const errors = get(data, 'response.data.errors', []);
+      const errors = get(data, 'errors', []);
 
       if (errors.length > 0) {
-        errors.map((error) => toast.error(error));
+        //   errors.map((error) => toast.error(error));
       } else {
         toast.error('Erro deconhecido');
       }
 
-      if (status === 401) dispacth(actions.loginRequest());
+      if (status === 401) dispacth(actions.loginFailure());
     }
   };
 
@@ -139,7 +143,16 @@ export default function Aluno({ match }) {
     <Container>
       <Loading isLoading={isLoading} />
 
-      <h1>{id ? 'Editar aluno' : 'Novo aluno'}</h1>
+      <Title>{id ? 'Editar aluno' : 'Novo Aluno'}</Title>
+
+      {id && (
+        <ProfilePicture>
+          {foto ? <img src={foto} alt={nome} /> : <FaUserCircle size={180} />}
+          <Link to={`/fotos/${id}`}>
+            <FaEdit size={24} />
+          </Link>
+        </ProfilePicture>
+      )}
 
       <Form onSubmit={handleSubmit}>
         <input
